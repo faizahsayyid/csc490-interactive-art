c13bf56b66ff8096803734dc73d0c4ddfe8bc90c
Irene Kang <irene.kang@mail.utoronto.ca>
2024-03-31 16:22:26 -0400

    Added project related views

--- server/arduino/views.py
+++ server/arduino/views.py
@@ -6,6 +6,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.utils.decorators import method_decorator
 from .utils.ino_code_ds import inoCodeDataStructure
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from .models import Project
+from .serializers import ProjectSerializer
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SendCodeToBoard(APIView):
@@ -44,5 +48,94 @@ class Demo(APIView):
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": str(e)})
         
-        
 
+@method_decorator(csrf_exempt, name='dispatch')
+@method_decorator(login_required, name='dispatch')
+class ProjectListView(View):
+    def get(self, request):
+        """
+        Get a list of all projects owned by the user
+        """
+        try:
+            projects = Project.objects.filter(owner=request.user)
+            serializer = ProjectSerializer(projects, many=True)
+            return JsonResponse(serializer.data, safe=False)
+            
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+    def post(self, request):
+        """
+        Create a new project
+        """
+        try:
+            # Get data from request body
+            data = json.loads(request.body)
+            project_name = data.get('project_name', None)
+            if not project_name:
+                return JsonResponse({'error': 'Project name is required'}, status=400)
+
+            # Check if project with the same name already exists
+            if Project.objects.filter(project_name=project_name, owner=request.user).exists():
+                return JsonResponse({'error': 'Project with this name already exists'}, status=400)
+
+            # Create new project
+            project = Project.objects.create(owner=request.user, project_name=project_name)
+            # Serialize the updated list
+            serializer = ProjectSerializer(Project.objects.filter(owner=request.user), many=True)
+            return JsonResponse(serializer.data, safe=False)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+# EndPoint: /project/<project_id> 
+class ProjectDetailView(View):
+    def get(self, request, project_id):
+        """
+        Get details of a specific project
+        """
+        try:
+            project = Project.objects.get(id=project_id, owner=request.user)
+            serializer = ProjectSerializer(project)
+            return JsonResponse(serializer.data)
+        except Project.DoesNotExist:
+            return JsonResponse({'error': 'Project not found'}, status=404)
+
+    def delete(self, request, project_id):
+        """
+        Delete a project
+        """
+        try:
+            project = Project.objects.get(id=project_id, owner=request.user)
+            project.delete()
+            return JsonResponse({'success': True, 'message': 'Project deleted successfully'})
+        except Project.DoesNotExist:
+            return JsonResponse({'error': 'Project not found'}, status=404)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    
+    def put(self, request, project_id):
+        """
+        Update a project
+        """
+        try:
+            project = Project.objects.get(id=project_id, owner=request.user)
+            data = json.loads(request.body)
+
+            # if the user is trying to edit the project name
+            if 'project_name' in data:
+                project_name = data.get('project_name', None)
+                if not project_name:
+                    return JsonResponse({'error': 'Project name is required'}, status=400)
+                project.project_name = project_name
+            # TODO: Add more fields to update
+            if 'connections' in data:
+                connections = data.get('connections', None)
+                if not connections:
+                    return JsonResponse({'error': 'Connections are required'}, status=400)
+                project.connections = connections
+            project.save()
+            serializer = ProjectSerializer(project)
+            return JsonResponse(serializer.data)
+        
+        except Project.DoesNotExist:
+            return JsonResponse({'error': 'Project not found'}, status=404)
