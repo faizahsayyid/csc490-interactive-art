b390c09ee980c33d007e35f3e6dbcfbd5e552f25
Irene Kang <irene.kang@mail.utoronto.ca>
2024-03-31 16:22:47 -0400

    Added urls

--- server/arduino/urls.py
+++ server/arduino/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SendCodeToBoard, Demo
+from .views import SendCodeToBoard, Demo, ProjectListView, ProjectDetailView
 
 urlpatterns = [
     path('api/', SendCodeToBoard.as_view()),
-    path('demo/', Demo.as_view())
+    path('demo/', Demo.as_view()),
+    path('projects/', ProjectListView.as_view()),
+    path('projects/<int:pk>/', ProjectDetailView.as_view()),
 ]
