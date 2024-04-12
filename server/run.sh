59126e4e8fc5a538a5b4d92c782c7fd72e7f3134
Irene Kang <irene.kang@mail.utoronto.ca>
2024-04-11 21:26:15 -0400

    shell script

---
+++ server/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install python3.8-venv -y
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install django pillow djangorestframework djangorestframework-simplejwt django-environ django-cors-headers
+pip install -r requirements.txt
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver
