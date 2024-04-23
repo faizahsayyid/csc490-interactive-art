24d156cd81c5b4d702838c423bb0d5e27567d8e8
Irene Kang <irene.kang@mail.utoronto.ca>
2024-04-23 17:08:58 -0400

    Replaced the prop isLoggedIn as the state as it stopped working[temporary]

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { logout } from "../api/auth";
-import { useMutation } from "@tanstack/react-query";
+// import { logout } from "../api/auth";
+// import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { API_URL } from "../api/config";
+
 
 type HeaderProps = {
   isLoggedIn: boolean;
 };
 
-export const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
-  const logoutMutation = useMutation({ mutationFn: logout });
+export const Header: React.FC<HeaderProps> = ({ }) => {
+  // const logoutMutation = useMutation({ mutationFn: logout });
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const onLogout = () => {
-    logoutMutation.mutate();
-    navigate("/login");
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }
+  , [isLoggedIn]);
+
+  const onLogout = async () => {
+    try {
+      const response = await fetch(`${API_URL}/accounts/logout/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const data = await response.json();
+      if (data.status === 400) {
+        console.log("error", data);
+        return;
+      }
+
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    } catch (error) {
+      console.error("error", error);
+    }
   };
 
   return (
